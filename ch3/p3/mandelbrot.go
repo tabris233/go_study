f3657b874ae3c1e675fe167f8c9f67247ba60f97
tabris233 <604925267@qq.com>
2021-02-07 14:05:55 +0000

    <=ch3.p5

---
+++ ch3/p3/mandelbrot.go
@@ -0,0 +1,65 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+    "image"
+    "image/color"
+    "image/png"
+    "math/cmplx"
+    "os"
+)
+
+
+func main() {
+    const (
+        xmin, ymin, xmax, ymax = -2, -2, +2, +2
+        width, height          = 1024, 1024
+    )
+
+    img := image.NewRGBA(image.Rect(0, 0, width, height))
+    for py := 0; py < height; py++ {
+        y := float64(py)/height*(ymax-ymin) + ymin
+        for px := 0; px < width; px++ {
+            x := float64(px)/width*(xmax-xmin) + xmin
+            z := complex(x, y)
+            // Image point (px, py) represents complex value z.
+            img.Set(px, py, mandelbrot(z))
+        }
+    }
+    png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+    const iterations = 200
+    const contrast = 15
+
+    var v complex128
+    for n := uint8(0); n < iterations; n++ {
+        v = v*v + z
+        if cmplx.Abs(v) > 2 {
+			return getColor(n)
+		}
+	}
+	return color.Black
+}
+func getColor(n uint8) color.Color {
+	paletted := [16]color.Color{
+		color.RGBA{66, 30, 15, 255},    // # brown 3
+		color.RGBA{25, 7, 26, 255},     // # dark violett
+		color.RGBA{9, 1, 47, 255},      //# darkest blue
+		color.RGBA{4, 4, 73, 255},      //# blue 5
+		color.RGBA{0, 7, 100, 255},     //# blue 4
+		color.RGBA{12, 44, 138, 255},   //# blue 3
+		color.RGBA{24, 82, 177, 255},   //# blue 2
+		color.RGBA{57, 125, 209, 255},  //# blue 1
+		color.RGBA{134, 181, 229, 255}, // # blue 0
+		color.RGBA{211, 236, 248, 255}, // # lightest blue
+		color.RGBA{241, 233, 191, 255}, // # lightest yellow
+		color.RGBA{248, 201, 95, 255},  // # light yellow
+		color.RGBA{255, 170, 0, 255},   // # dirty yellow
+		color.RGBA{204, 128, 0, 255},   // # brown 0
+		color.RGBA{153, 87, 0, 255},    // # brown 1
+		color.RGBA{106, 52, 3, 255},    // # brown 2
+	}
+	return paletted[n%16]
+}
